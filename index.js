e5e910b36b349c56643ac1b63c33c472f9c7a7a6
Joe Jiang <hijiangtao@gmail.com>
2017-11-13 22:05:05 +0800

    update: init front-end page draft

--- index.js
+++ index.js
@@ -1,12 +1,26 @@
 require('babel-core/register');
 
 const Koa = require('koa');
+const logger = require('koa-logger');
+const views = require('koa-views');
+const path = require('path');
+
 const router = require('./router.js');
 
-const app = new Koa();
+const app = module.exports = new Koa();
+
+app.use(logger());
+
+app.use(views(path.join(__dirname, '/views'), {
+    extension: 'ejs'
+}));
+
+// app.use(async(ctx) => {
+//     await ctx.render('home', {});
+// });
 
 app
     .use(router.routes())
     .use(router.allowedMethods());
 
-app.listen(3000);
+if (!module.parent) app.listen(3000);
--- router.js
+++ router.js
@@ -3,6 +3,9 @@ const router = new Router();
 import * as api from './api/v1/index.js';
 
 router
+    .get('/home', async(ctx) => {
+        await ctx.render('home', {});
+    })
     .get('/testGraph', api.testGraph)
     .all('/basicGraph', api.basicGraph);
 
