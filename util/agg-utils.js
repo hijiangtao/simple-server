ee2e80a27736cf3d0ff907cf604f0764eae6d78b
Joe Jiang <hijiangtao@gmail.com>
2017-11-30 18:11:36 +0800

    update: release connection after get the response

--- util/agg-utils.js
+++ util/agg-utils.js
@@ -54,7 +54,7 @@ const queryGraph = async(pool, queryparams) => {
         rawNodes = await queryElements(connection, 'nodes', queryInput),
         rawEdges = await queryElements(connection, 'edges', queryInput);
 
-    // return resToGraph(rawNodes, rawEdges);
+    pool.close()
     return {
         "nodes": rawNodes,
         "edges": rawEdges,
@@ -79,6 +79,7 @@ const queryElements = async(conn, type, params) => {
                 reject(err);
             }
 
+            conn.release();
             resolve(res);
         });
     })
